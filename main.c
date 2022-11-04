42e8e71e253f41cfbe1ca87d67140f3a5a69eafb
Carrol2709 <111042693+Carrol2709@users.noreply.github.com>
2022-11-04 09:59:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void sig_handler(int sig)
 /**
  * execute - Executes a command in a child process.
  * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
+ * @front: A double pointer to the beginning of args
  *
  * Return: If an error occurs - a corresponding error code.
  *         O/w - The exit value of the last executed command.
